f103df25b6891db69ae7d549eb5851a60c27bb32
clearluo <tolsw@qq.com>
2018-02-10 13:54:09 +0800

    add tool

---
+++ moneyTool/monty_tool.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	etf300()
+}
+
+/*
+ 每月定存指数基金养老计划
+*/
+func etf300() {
+	var monthMondy float64 = 0.1 // 每月定存多少W
+	var yearCount int = 30       // 定存年数
+	var yearRate float64 = 0.15  // 平均年华收益率
+	var sum float64 = 0          // 总收益
+	for i := 1; i <= yearCount; i++ {
+		fmt.Printf("第%d年每月存入:%f\n", i, monthMondy)
+		for j := 0; j < 12; j++ {
+			sum += monthMondy
+			sum = (sum*yearRate)/12 + sum
+		}
+		fmt.Printf("第%d年后总金额:%.2fW\n", i, sum)
+	}
+}
