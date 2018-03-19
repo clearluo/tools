504c4f0538d9592c899cbc111e2fb32c5b8b560f
clearluo <tolsw@qq.com>
2018-03-19 10:46:47 +0800

    xx

--- moneyTool/monty_tool.go
+++ moneyTool/monty_tool.go
@@ -9,15 +9,15 @@ func main() {
 }
 
 /*
- 每月定存指数基金养老计划
+ 每月定存etf300指数基金计划
 */
 func etf300() {
 	var monthMondy float64 = 0.1 // 每月定存多少W
 	var yearCount int = 30       // 定存年数
-	var yearRate float64 = 0.15  // 平均年华收益率
+	var yearRate float64 = 0.1   // 平均年化复合收益率
 	var sum float64 = 0          // 总收益
 	for i := 1; i <= yearCount; i++ {
-		fmt.Printf("第%d年每月存入:%f\n", i, monthMondy)
+		//fmt.Printf("第%d年每月存入:%f\n", i, monthMondy)
 		for j := 0; j < 12; j++ {
 			sum += monthMondy
 			sum = (sum*yearRate)/12 + sum
