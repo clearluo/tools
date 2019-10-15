d4605256f2ac6f49b0ec0ca8aae9a53056a407b9
clearluo <tolsw@qq.com>
2019-10-15 19:25:27 +0800

    add

--- moneyTool/money_tool.go
+++ moneyTool/money_tool.go
@@ -10,14 +10,13 @@ func main() {
 	// fmt.Printf("年化利率：%.2f%%\n", InstallmentCal(200000, 36, 7380))
 }
 
-/*
- * 功能：每月定投股市收益计算
- * monthMoney：初使每月定存多少W
- * yearInc：每月投入以多少比例增加(以年为单位递增，如：第一年每月投入1k，以0.05递增,那么第二年每月投入1.05k,第二年每月投入1.1025k)
- * yearCount：定存年数30
- * yearRate：期望平均年化复合收益率0.15
- * 返回值：总收入W
- */
+// Etf 每月定投股市收益计算
+// monthMoney：初使每月定存多少W
+// yearInc：每月投入以多少比例增加
+// (以年为单位递增，如：第一年每月投入1k，以0.05递增,那么第二年每月投入1.05k,第二年每月投入1.1025k)
+// yearCount：定存年数30
+// yearRate：期望平均年化复合收益率0.15
+// 返回值：总收入W
 func Etf(monthMoney float64, yearInc float64, yearCount int, yearRate float64) float64 {
 	var sumBase float64 = 0 // 总本钱
 	var sum float64 = 0     // 总收益
@@ -26,8 +25,8 @@ func Etf(monthMoney float64, yearInc float64, yearCount int, yearRate float64) f
 		sumBase += (monthMoney * 12)
 		for j := 1; j <= 12; j++ {
 			sum += monthMoney
-			sum += (sum*yearRate)/12
-			fmt.Printf("第%d年,第%d个月:%.2fW\n", i,j, sum)
+			sum += (sum * yearRate) / 12
+			fmt.Printf("第%d年,第%d个月:%.2fW\n", i, j, sum)
 		}
 		monthMoney *= (1 + yearInc)
 		//fmt.Printf("第%d年后投入总金额:%.2fW\n", i, sumBase)
@@ -39,14 +38,12 @@ func Etf(monthMoney float64, yearInc float64, yearCount int, yearRate float64) f
 	return sum
 }
 
-/*
- * 功能：根据贷款总额和每月还款金额计算实际贷款年化利率
- * totalMoney:贷款总额(单位：元)
- * monthCount:期数
- * monthMoney:每期还款额(单位：元)
- * 返回值：年化利率
- * 每月还款额=[贷款本金×月利率×（1+月利率）^还款月数]÷[（1+月利率）^还款月数－1]
- */
+// InstallmentCal 根据贷款总额和每月还款金额计算实际贷款年化利率
+// totalMoney:贷款总额(单位：元)
+// monthCount:期数
+// monthMoney:每期还款额(单位：元)
+// 返回值：年化利率
+// 每月还款额=[贷款本金×月利率×（1+月利率）^还款月数]÷[（1+月利率）^还款月数－1]
 func InstallmentCal(totalMoney float64, monthCount int, monthMoney float64) float64 {
 	// 采用最土的暴力破解法
 	var rate float64 = 0.0200 // 从2%开始算起，保留两位小数
