e80507cad84d6a98e6eade87b17cddf8034630be
clearluo <tolsw@qq.com>
2020-01-15 14:19:47 +0800

    m

--- tool/dbd/main.go
+++ tool/dbd/main.go
@@ -17,33 +17,59 @@ type Ret struct {
 	Message string      `json:"message"`
 }
 type Data struct {
-	AuctionId         int         `json:"auctionId"` // 商品id
-	AuctionRecordId   int         `json:"auctionRecordId"`
-	CurrentPrice      float64     `json:"currentPrice"` // 当前价格
-	Num               int         `json:"num"`
-	CurrentBidder     string      `json:"currentBidder"` // 当前最高价用户
-	BidderNickName    string      `json:"bidderNickName"`
-	BidderImage       interface{} `json:"bidderImage"`
-	Status            interface{} `json:"status"`
-	OfferPrice        interface{} `json:"offerPrice"`
-	ActualEndTime     int64       `json:"actualEndTime"` // 结束时间
-	DelayCount        int         `json:"delayCount"`
-	VirtualDelayCount int         `json:"virtualDelayCount"`
-	SpectatorCount    interface{} `json:"spectatorCount"`
-	UserNum           interface{} `json:"userNum"`
+	AuctionId       int         `json:"auctionId"` // 商品id
+	AuctionRecordId int         `json:"auctionRecordId"`
+	CurrentPrice    float64     `json:"currentPrice"` // 当前价格
+	Num             int         `json:"num"`
+	CurrentBidder   string      `json:"currentBidder"` // 当前最高价用户
+	BidderNickName  string      `json:"bidderNickName"`
+	BidderImage     interface{} `json:"bidderImage"`
+	Status          interface{} `json:"status"`
+	OfferPrice      interface{} `json:"offerPrice"`
+	ActualEndTime   int64       `json:"actualEndTime"` // 结束时间
+	//DelayCount        int         `json:"delayCount"`
+	//VirtualDelayCount int         `json:"virtualDelayCount"`
+	//SpectatorCount    interface{} `json:"spectatorCount"`
+	//UserNum           interface{} `json:"userNum"`
 }
 
 const (
 	userId   = "t***w"
-	id       = "200590046"
-	name     = "MagicWatch2"
-	maxPrice = 900
-	addPrice = 5 // 加价间隔
+	id       = "200590748"
+	name     = "AipPods"
+	maxPrice = 99
+	addPrice = 3 // 加价间隔
 	cookie   = "shshshfpa=b8cc7f40-5852-c878-2008-202cb7b564aa-1576751256; shshshfpb=b8cc7f40-5852-c878-2008-202cb7b564aa-1576751256; __jdv=104464258|direct|-|none|-|1578034862589; mba_muid=1576642527839566679144; 3AB9D23F7A4B3C9B=MX3IRQ3GTOJTZWGF3WHRPHINBUVMCNIWU5CQR7J25IYYFJO5JJ57QBXR3KLXSC56VGZEDIZ3AURLP5S44YW3HXVQYY; shshshfp=e3c1b507580913054efdb15be6c61cc9; areaId=16; ipLoc-djd=16-1303-3484-0; __jdc=104464258; __tak=3abcee8b2e24588670bf5a131f4c8892fd4ad34146e8732717e121ab22b10584feb8b824eb5722adce847eb15324814d875b636e3659ff2a9ec56ee11664ad9f6b25b8704d6586cb96c41341926130c3; __jda=104464258.1576642527839566679144.1576642528.1579049397.1579054173.20; thor=A6CD0F6C27D13A138EBD4D18B8A97E3C62A6F403197B591A1AFFF8FC4B42DF94247A82A6D78E88DC9F90945E40FCB265C9D16B4753A3C6F31059A5F85E04EBC60C672D676A204094FAA4D23C50B624B6660CC7C216544E9714BFB963BD0AA561B4879696FF97E6A39237F80B18FDB1A666910DD82DD0C4703575F2C1BBF0E3B8; pin=tolsw; unick=%E6%B0%91%E9%97%B4%E5%8D%97%E5%AF%92%E5%B8%A6; __jdb=104464258.15.1576642527839566679144|20.1579054173"
 	token    = "ak8p224mjwbtbxwuwbm15790493966694zcg~NmZeSyVEbFNSdHB0dFRaCnlxAw1mRHpTBiUjb35DFm5vLUROOBEzLUF7G28iAAFBKBgVFA1EPwIVKDclGENXbm8iVlQiAwpTTx1lKSsTCG5vfmsaIx8/FV4dZWEYQwtub35rGmBRYUMCcCV0cAVbC3h1Bgs3X24QVnkhLiVUWAB9cgYOZgA7FD9jaxFmCB5fEWYNZHMANx0QJBtvaD1PWj4waxprOnQCBi0rYzQABEIsLRlbPgsKU08dZT0qPU8IEWYYWSQFIhgML2opIRUMWyFrBQhhU29BU3VxEWZNMRA9MGsaazp0GRc1Nz5+PU8eEWZHUQ1EbC1Bc3VhfE1bHn1oBxRlXwpTHmNrbyEFCUEqZg0aaFA3SEFtZS43Q1cQeHEATjkQIAIUdSMsLxUbWXgsA1lhUTVGVHkvK3wFWgB+K0NJc0p0EkF7ZSsgWB5RI31ECjpTJwIOOSR/M1NcB3pyAQxjU2RGVnlzeykSBxBhZlNLP0RsUwcoITdwUhVeb2gVUSJEbFNSY2tvLggOEHdmDgFoXnQM|~1579057846361~1~20191203~eyJ2aXdlIjoiMCIsImJhaW4iOnsiaWMiOiIxIiwibGUiOiIxMDAiLCJjdCI6IjAiLCJkdCI6ImkifX0=~1~~30v4|1d6w-hb,8b;doei:,1,1,0,0,1,1000,-1000,1000,-1000;dmei:,1,1,1,1000,-1000,1000,-1000,1000,-1000;emc:,d:1;emcf:,d:1;ivli:;iivl:;ivcvj:;scvje:;1579057845453,1579057846357,0,0,1,1,0,0,0,0,0;uk67"
 )
 
+var (
+	priceData   = url.Values{} // 出价的body
+	priceClient = &http.Client{}
+	priceHead   = &http.Request{}
+	priceUrlStr = ""
+)
+
 func init() {
+	priceData.Set("trackId", "0d1b9bc80e5339d06cd27454b25c4b66")
+	priceData.Set("eid", "MX3IRQ3GTOJTZWGF3WHRPHINBUVMCNIWU5CQR7J25IYYFJO5JJ57QBXR3KLXSC56VGZEDIZ3AURLP5S44YW3HXVQYY")
+	priceData.Set("auctionId", id)
+	priceData.Set("token", token)
+	//priceData.Set("price", fmt.Sprintf("%v", price))
+	priceData.Set("entryid", "")
+	priceData.Set("address", "16-1303-48712-48759")
+	priceData.Set("initFailed", "false")
+
+	apiUrl := "https://used-api.paipai.com/auctionRecord/offerPrice"
+	//priceData.Set("price", fmt.Sprintf("%v", price))
+	u, _ := url.ParseRequestURI(apiUrl)
+	priceUrlStr = u.String()
+	priceHead, _ = http.NewRequest("POST", priceUrlStr, strings.NewReader(priceData.Encode())) // URL-encoded payload
+	priceHead.Header.Add("Referer", "https://sell.paipai.com/auction-detail/"+id)
+	priceHead.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36")
+	priceHead.Header.Add("cookie", cookie)
+	priceHead.Header.Add("content-type", "application/x-www-form-urlencoded")
+
 }
 
 func main() {
@@ -75,22 +101,24 @@ func main() {
 		}
 		if remainTime < 10 {
 			sleepTime = time.Second
+			bidPrice(ret.CurrentPrice + addPrice)
 		}
 		if remainTime < 2 {
 			isSleep = false
-			price := ret.CurrentPrice + 3
+			price := ret.CurrentPrice + addPrice
 			retCode := 0
 			for price < maxPrice {
 				retCode = bidPrice(price)
 				if retCode == 302 {
-					time.Sleep(time.Millisecond * 50) // 休眠1/50秒
-					continue                          // 连续出价不抬价
+					continue // 连续出价不抬价
 				}
-				price += 3
+				price += addPrice
 			}
 
 		} else {
-			//bidPrice(ret.CurrentPrice + addPrice)
+			start := time.Now()
+			bidPrice(ret.CurrentPrice + addPrice)
+			fmt.Println("出价用时:", time.Since(start))
 		}
 	}
 	if ret, err := getPrice(); err != nil {
@@ -138,7 +166,7 @@ func getPrice() (*Data, error) {
 
 // code=302,message=同一用户不能连续出价
 // code=304,message=客官，您出价太低了，再加点吧！
-func bidPrice(price float64) int {
+func bidPriceOld(price float64) (retCode int) {
 	apiUrl := "https://used-api.paipai.com/auctionRecord/offerPrice"
 	data := url.Values{}
 	data.Set("trackId", "0d1b9bc80e5339d06cd27454b25c4b66")
@@ -150,21 +178,17 @@ func bidPrice(price float64) int {
 	data.Set("address", "16-1303-48712-48759")
 	data.Set("initFailed", "false")
 	u, _ := url.ParseRequestURI(apiUrl)
-	//u.Path = resource
 	urlStr := u.String()
-
 	client := &http.Client{}
 	r, _ := http.NewRequest("POST", urlStr, strings.NewReader(data.Encode())) // URL-encoded payload
 	r.Header.Add("Referer", "https://sell.paipai.com/auction-detail/"+id)
 	r.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36")
 	r.Header.Add("cookie", cookie)
 	r.Header.Add("content-type", "application/x-www-form-urlencoded")
-	//r.Header.Add("content-length", strconv.Itoa(len(data.Encode())))
-
 	resp, err := client.Do(r)
 	if err != nil {
 		//fmt.Println(err.Error())
-		return -1
+		return
 	}
 	defer resp.Body.Close()
 	retByte, _ := ioutil.ReadAll(resp.Body)
@@ -173,13 +197,48 @@ func bidPrice(price float64) int {
 	err = json.Unmarshal(retByte, ret)
 	if err != nil {
 		//fmt.Println(err)
-		return -1
+		return
 	}
-	if ret.Code != 200 {
+	retCode = ret.Code
+	if retCode != 200 {
 		err := fmt.Errorf("%v:%v", ret.Code, ret.Message)
 		fmt.Println(err)
-		return ret.Code
+		return
 	}
 	fmt.Println("出价成功:", price)
-	return ret.Code
+	return
+}
+
+func bidPrice(price float64) (retCode int) {
+	getReqHead(price)
+	resp, err := priceClient.Do(priceHead)
+	if err != nil {
+		//fmt.Println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	retByte, _ := ioutil.ReadAll(resp.Body)
+	ret := &Ret{}
+	err = json.Unmarshal(retByte, ret)
+	if err != nil {
+		//fmt.Println(err)
+		return
+	}
+	retCode = ret.Code
+	if retCode != 200 {
+		err := fmt.Errorf("%v:%v", ret.Code, ret.Message)
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("出价成功:", price)
+	return
+}
+
+func getReqHead(price float64) {
+	priceData.Set("price", fmt.Sprintf("%v", price))
+	priceHead, _ = http.NewRequest("POST", priceUrlStr, strings.NewReader(priceData.Encode())) // URL-encoded payload
+	priceHead.Header.Add("Referer", "https://sell.paipai.com/auction-detail/"+id)
+	priceHead.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36")
+	priceHead.Header.Add("cookie", cookie)
+	priceHead.Header.Add("content-type", "application/x-www-form-urlencoded")
 }
