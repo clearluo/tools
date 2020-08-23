79455070ead4a210a799a1aa4266d6ce353acaaf
clearluo <tolsw@qq.com>
2020-08-23 08:36:16 +0800

    modify

--- tool/go.mod
+++ tool/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/lestrrat-go/strftime v1.0.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	github.com/urfave/cli v1.22.4
 	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
 	golang.org/x/sys v0.0.0-20200819171115-d785dc25833f // indirect
 	golang.org/x/text v0.3.3 // indirect
--- tool/main.go
+++ tool/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"os"
 	"tool/common/basic/runBefore"
+	"tool/dbd"
 	"tool/finance"
+
+	"github.com/urfave/cli"
 )
 
 func init() {
@@ -10,6 +14,28 @@ func init() {
 }
 
 func main() {
-	//dbd.Dbd()
+	app := cli.NewApp()
+	app.Name = "tool"
+	app.Author = "clearluo"
+	app.Version = "1.0.0"
+	app.Usage = "./tool dbd|finance"
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:   "dbd",
+			Action: doDbd,
+		},
+		cli.Command{
+			Name:   "finance",
+			Action: doFinance,
+		},
+	}
+	app.Run(os.Args)
+}
+func doDbd(ctx *cli.Context) error {
+	dbd.Dbd()
+	return nil
+}
+func doFinance(ctx *cli.Context) error {
 	finance.Finance()
+	return nil
 }
